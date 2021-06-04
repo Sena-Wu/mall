282d57cb61a3b14e6ca1de2d7e6fd9520a3f6503
wu <527869722@qq.com>
2021-06-04 23:37:53 +0800

    feat: 增加logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 # -*- coding:utf-8 -*-
 # Author:wu
-import os
+
+import logging.config
 
 from flask import Flask
 
-from .utils.conf import read_yaml
+from .utils.get_conf import get_env_config, get_log_config
 from .utils.jsonencoder import CustomJSONEncoder
 from .utils.result import Res
 
@@ -13,30 +14,31 @@ def create_app(env=None):
     from . import db, account, order, commodity
     app = Flask(__name__)
 
-    # 读取配置文件
-    pwd = os.getcwd()
-    if not env:
-        config_path = os.path.join(pwd, 'conf{}dev.yaml'.format(os.sep))
-    else:
-        config_path = os.path.join(pwd, 'conf{}{}.yaml'.format(os.sep, env))
+    # 日志设置
+    log_conf = get_log_config()
+    logging.config.dictConfig(log_conf)
+    logger = logging.getLogger('root')
 
-    conf = read_yaml(config_path)
-    app.config.update(conf)
+    # 读取配置文件
+    env_conf = get_env_config()
+    app.config.update(env_conf)
+
     # 替换默认的json编码器
     app.json_encoder = CustomJSONEncoder
 
     # 处理404异常
     @app.errorhandler(404)
     def page_not_found(e):
-        # todo 打印日志
+        logger.error(e)
         return Res.fail(404)
 
     # 全局错误处理
     @app.errorhandler(Exception)
     def framework_error(e):
-        # todo 打印日志
+        logger.error(e)
         return Res.fail("server error")
 
     app.register_blueprint(account.account)
     app.register_blueprint(commodity.commodity)
+
     return app
