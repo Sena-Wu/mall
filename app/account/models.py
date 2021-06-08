57415987cb30bb34d83dd754e87d66b057b7197f
wu <527869722@qq.com>
2021-06-08 16:01:33 +0800

    fix: 完善get、post请求

--- app/account/models.py
+++ app/account/models.py
@@ -1,2 +1,87 @@
 # -*- coding:utf-8 -*-
 # Author:wu
+
+from datetime import datetime
+
+from .. import db
+from ..utils.error_class import Insert_Error
+
+
+# noinspection SpellCheckingInspection
+class Account(db.Model):
+    # 声明表名
+    __tablename__ = 'account_tb'
+    # 建立字段函数
+    id = db.Column(db.Integer, primary_key=True)
+    account_name = db.Column(db.String(200))
+    head_url = db.Column(db.String(200))
+    cell_phone = db.Column(db.String(200))
+    addr = db.Column(db.String(200))
+    sex = db.Column(db.String(200))
+    birthday = db.Column(db.String(200))
+    money = db.Column(db.DECIMAL())
+    create_time = db.Column(db.DateTime())
+    update_time = db.Column(db.DateTime())
+
+
+def get_by_id(account_id: int) -> list:
+    """
+    根据id获取账户
+    :param account_id: 账户id
+    :return: 账户信息
+    """
+    acc_list = []
+    query = Account.query
+    acc = query.filter(Account.id == account_id).with_entities(Account.id, Account.account_name).first()
+
+    if acc:
+        acc_list.append({
+            "id": acc.id,
+            "account_name": acc.account_name
+        })
+    return acc_list
+
+
+def list_by_params(params: dict) -> list:
+    """
+    分页查询账户数据
+    :param params: 查询参数
+    :return: 分页列表
+    """
+    acc_list = []
+    query = Account.query
+    query = query.filter(Account.account_name.like('%{}%'.format(params['account_name'])))
+
+    order_value = Account.__dict__.get(params['order_value'])
+    order_by = getattr(order_value, params['order_type'])()
+
+    account_list = query.order_by(order_by).offset(params['from']).limit(params['size']).all()
+
+    for acc in account_list:
+        acc_list.append({
+            "id": acc.id,
+            "account_name": acc.account_name
+        })
+    return acc_list
+
+
+def add_by_params(params: dict) -> list:
+    acc_list = []
+    params['create_time'] = params['update_time'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    acc = Account(**params)
+
+    db.session.add(acc)
+    try:
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        raise Insert_Error(e)
+
+    acc_list.append({
+        "id": acc.id,
+        "account_name": acc.account_name
+    })
+    return acc_list
+
+def update_by_params(params: dict) -> list:
+    pass
--- app/account/route.py
+++ app/account/route.py
@@ -2,55 +2,69 @@
 # Author:wu
 
 import logging
+import random
 
-from flask import request, current_app
+from flask import request
 
-from . import account
-from ..utils.result import Res
+from app.account import account
+from app.account.models import get_by_id, list_by_params, add_by_params, update_by_params
+from app.utils.result import Res
+from app.utils.generate_random import random_str
+from datetime import datetime, date
 
+alphabet = 'abcdefghijklmnopqrstuvwxyz!@#$%^&*()0123456789'
 logger = logging.getLogger("root")  # 创建日志实例
 
 
-# 强制参数类型<int:account_id>;http://127.0.0.1:5000/accounts/123
-@account.route(rule='/<int:account_id>', methods=['GET'])  # <int: account_id>ValueError: malformed url rule
+@account.route(rule='/<int:account_id>', methods=['GET'])
 def get_account(account_id: int):
-    logger.info("account_id = " + str(account_id))
-    data = {
-        "url": request.url_rule,
-        "methods": request.method
-    }
+    data = get_by_id(account_id)
     return Res.success(data=data)
 
 
-# 请求格式http://127.0.0.1:5000/accounts/?xbox; http://127.0.0.1:5000/accounts/
 @account.route(rule='/', methods=['GET'])
 def list_account():
-    logger.info("this is info")
-    logger.debug("this is debug")
-    logger.error("this is error")
-    data = {
-        "url": request.full_path,
-        "methods": request.method
+    req_data = request.args
+    params = {
+        "from": req_data.get('from', 0),
+        "size": req_data.get('size', 10),
+        "account_name": req_data.get('account_name', ''),
+        "order_value": req_data.get('order_value', "create_time"),
+        "order_type": req_data.get('order_type', "desc"),
     }
+
+    data = list_by_params(params)
     return Res.success(data=data)
 
 
-@account.route(rule='', methods=['POST'])
+@account.route(rule='/', methods=['POST'])
 def add_account():
-    current_app.logger.info('111')
-    data = {
-        "url": request.full_path,
-        "methods": request.method
+    account_name = random_str(10)
+    # account_name = ''
+    req_data = request.args
+    params = {
+        'account_name': req_data.get('account_name', account_name),
+        'addr': req_data.get('addr', 'unknown'),
+        'cell_phone': req_data.get('cell_phone', 'unknown'),
+        'head_url': req_data.get('head_url', 'default.jpg'),
+        'sex': req_data.get('sex', 0),
+        'birthday': req_data.get('birthday', date.today()),
+        'money': req_data.get('money', '0'),
+        'create_time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        'update_time': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     }
+
+    data = add_by_params(params)
     return Res.success(data)
 
 
-@account.route(rule='/<account_id>', methods=['PUT'])
+@account.route(rule='/', methods=['PUT'])
 def update_account():
-    data = {
-        "url": request.full_path,
-        "methods": request.method
-    }
+    req_data = request.args
+    if req_data['id'] or req_data['account_name']:
+        params = req_data
+        data = update_by_params(params)
+
     return Res.success(data)
 
 
