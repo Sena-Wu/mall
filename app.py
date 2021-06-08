6dd283195d13d0c95e8f0a66d53e4ccd8325b8a0
wu <527869722@qq.com>
2021-06-08 21:56:50 +0800

    fix: 1.应用manager接管app 2.新增create_synthetic_data命令

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
+from flask_script import Manager
+from app.utils.create_accounts import create_synthetic_data
 from app import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run()
+
+    app.app_context().push()
+    manager = Manager(app=app)
+    manager.add_command('create_synthetic_data', create_synthetic_data())
+
+    manager.run()
