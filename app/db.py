4b75fb1c3dfce4080afc9ff2932640b55cc4cfb0
wu <527869722@qq.com>
2021-06-11 12:51:43 +0800

    fix: 格式化代码

--- app/db.py
+++ app/db.py
@@ -10,38 +10,3 @@
 # <模型类>.query.<过滤方法>.<查询方法>
 # Account.query.filter(Account.id == 123).all()
 # Account.query.all()
-from . import db
-
-def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
-
-    return g.db
-
-
-def close_db(e=None):
-    db = g.pop('db', None)
-
-    if db is not None:
-        db.close()
-
-
-def init_db(app):
-    db.init_app(app)
-
-
-@click.command('init-db')
-@with_appcontext
-def init_db_command():
-    """Clear the existing data and create new tables."""
-    init_db()
-    click.echo('Initialized the database.')
-
-
-def init_app(app):
-    app.teardown_appcontext(close_db)
-    app.cli.add_command(init_db_command)
